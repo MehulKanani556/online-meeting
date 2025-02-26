3761ddc6ce8549076db12b0e53fe6c2e2735cae7
jaykalathiya2005 <jaykalathiyainfotech@gmail.com>
2025-02-26 17:39:50 +0530

    aaa

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from "./Page/Home";
+import Login from './Page/Login';
 
 function App() {
   return (
 
     <Routes>
-      <Route path="/aaa" element={<Home />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
 
   );
